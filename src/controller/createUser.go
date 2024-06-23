798382c8157b96e709c10e7949fd1e264c78f058
Mateus Guedes <mxteusgued@gmail.com>
2024-06-23 16:52:17 -0300

    improve error validation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/MateusGuedess/go-crud/src/configuration/rest_err"
+	"github.com/MateusGuedess/go-crud/src/configuration/validation"
 	"github.com/MateusGuedess/go-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +12,10 @@ func CreateUser(c *gin.Context){
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()), nil)
+		errRest :=  validation.ValidateUserError(err)
 	
 	
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
