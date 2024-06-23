2ba92a0fce821601438d1d84407dbcdc0774d412
Mateus Guedes <mxteusgued@gmail.com>
2024-06-23 18:30:52 -0300

    made possible chang log level and output from env

--- src/configuration/logger/logger.go
+++ src/configuration/logger/logger.go
@@ -1,12 +1,18 @@
 package logger
 
 import (
+	"os"
+	"strings"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
 	log *zap.Logger
+
+	LOG_OUTPUT = "LOG_OUTPUT"
+	LOG_LEVEL = "LOG_LEVEL"
 )
 
 
@@ -14,8 +20,8 @@ func init() {
 	// Initialize logger
 
 	logConfig := zap.Config{
-		OutputPaths: []string{"stdout"},
-		Level: zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		OutputPaths: []string{getOutputLogs()},
+		Level: zap.NewAtomicLevelAt(getLevelLogs()),
 		Encoding: "json",
 		EncoderConfig: zapcore.EncoderConfig{
 			MessageKey: "message",
@@ -28,4 +34,28 @@ func init() {
 	}
 
 	log, _ = logConfig.Build()
+}
+
+func getOutputLogs () string {
+	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
+	if output == "" {
+		output = "stdout"
+	}
+	return output
+}
+
+func getLevelLogs() zapcore.Level {
+	level := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_LEVEL)))
+	switch level {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warn":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	default:
+		return zapcore.InfoLevel
+	}
 }
